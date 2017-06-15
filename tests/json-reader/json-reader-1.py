9d9935164ed063ec5e955cb33cee3162b5f2d724
Andreas Adelmann <andreas,adelmann@psi.ch>
2017-06-15 09:58:35 +0200

    test the json read and show how to use

---
+++ tests/json-reader/json-reader-1.py
@@ -0,0 +1,24 @@
+from pyOPALTools.optPilot import OptPilotJsonReader as optreader
+
+import sys,os
+
+
+try:
+    
+    # 1. Find all .json files of a directory, e.g. "./"
+    optjson = optreader.OptPilotJsonReader("./data/")
+    print ( "Read generation ", sys.argv[1])
+    # 2. Read in a generation file, e.g. 450  
+    optjson.readGeneration(sys.argv[1])
+    
+    # 3. Obtain an individual
+    print ( optjson.getDesignVariables() )
+    print ( optjson.getObjectives())
+#    var = optjson.getVariableNames()[1]
+#    print ( optjson.getIndividual(1, var) )
+    
+    # raise error
+#    print ( optjson.getIndividual(1, "bla") )
+
+except:
+    print ( sys.exc_info()[1] )
