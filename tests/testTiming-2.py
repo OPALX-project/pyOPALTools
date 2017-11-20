336dae41ad60fb91054cabbe298fe3d6614b7de6
Matthias Frey <matthias.frey@psi.ch>
2017-11-20 15:18:20 +0100

    Timing: add line plot

--- tests/testTiming-2.py
+++ tests/testTiming-2.py
@@ -2,6 +2,12 @@ import timing.TimePlot as TimePlot
 
 tplot = TimePlot()
 
-tplot.pie_plot(fname = 'testData/timing.dat', cmap='viridis_r')
+tplot.pie_plot(fname = 'testData/timings/timing_bdw_16cores.dat', cmap='viridis_r', first=7)
 
-tplot.summary_plot(fname='testData/timing.dat', grid=True)
+tplot.summary_plot(fname='testData/timings/timing_bdw_16cores.dat', grid=True)
+
+tplot.line_plot(['testData/timings/timing_bdw_1core.dat',
+                'testData/timings/timing_bdw_2core.dat',
+                'testData/timings/timing_bdw_8core.dat',
+                'testData/timings/timing_bdw_4core.dat',
+                'testData/timings/timing_bdw_16cores.dat'], first=5)
--- timing/TimePlot.py
+++ timing/TimePlot.py
@@ -7,6 +7,118 @@ import timing.Timing as timing
 
 class TimePlot:
     
+    def line_plot(self, fnames, **kwargs):
+        """
+        Create a plot of several timings containing same timers.
+        Plot the first N most time consuming timings. Timing files
+        do not need to be ordered according to the number of cores.
+        
+        Parameters
+        ----------
+        fnames              (str)  timing files
+        saveas              (str)   export the summary plot
+        figsize=(12, 9)             size of the figure
+        grid=False          (bool)  show grid
+        
+        Notes
+        -----
+        Throws an exception if a file does not exist.
+        
+        Returns
+        -------
+        None
+        """
+        
+        # get kwargs arguments
+        first = kwargs.get('first', None)
+        saveas = kwargs.get('saveas', None)
+        figsize = kwargs.get('figsize', (12, 9))
+        grid = kwargs.get('grid', False)
+        
+        # check if all files exist
+        cores = []
+        for fname in fnames:
+            if not os.path.isfile(fname):
+                raise RuntimeError("The file " + "'" + fname + "'" + " does not exist.")
+            
+            # sort file first (inefficient) according to cores
+            time = timing()
+            time.read_ippl_timing(fname)
+            cores.append(int(time.getTiming()[0]['cores']))
+        
+        cores_sorted, fnames_sorted = zip(*sorted(zip(cores, fnames),
+                                                      key=itemgetter(0),
+                                                      reverse=False))
+        
+        # read first entry
+        time = timing()
+        time.read_ippl_timing(fnames_sorted[0])
+        data = time.getTiming()
+        
+        # get first n most time consuming timing labels
+        _ , labels_sorted = self.__getMostTimeConsuming(first+1, data, 'cpu max', True)
+        
+        
+        tmin, tavg, tmax = self.__collect(data, labels_sorted)
+        
+        n = len(fnames_sorted)
+        x = np.linspace(0, n-1, n)
+        
+        fdata = {}
+        for i in range(len(tmin)):
+            fdata[labels_sorted[i]] = []
+            fdata[labels_sorted[i]] = {'min': [float(tmin[i])],
+                                       'max': [float(tmax[i])],
+                                       'avg': [float(tavg[i])]}
+        
+        # collect data (skip first since alread read)
+        for fname in fnames_sorted[1:]:
+            time.read_ippl_timing(fname)
+            data = time.getTiming()
+            
+            tmin, tavg, tmax = self.__collect(data, labels_sorted)
+            
+            for i in range(len(tmin)):
+                fdata[labels_sorted[i]]['min'].append(float(tmin[i]))
+                fdata[labels_sorted[i]]['max'].append(float(tmax[i]))
+                fdata[labels_sorted[i]]['avg'].append(float(tavg[i]))
+        
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        
+        for d in fdata:
+            ax.errorbar(cores_sorted, fdata[d]['avg'],
+                        yerr=[fdata[d]['min'], fdata[d]['max']], fmt='--o')
+        
+        ax.grid(grid)
+        plt.xlabel('#cores')
+        plt.ylabel('time [s]')
+        plt.xlim([0.5*cores_sorted[0], 1.05*cores_sorted[-1]])
+        ax.legend(labels_sorted, loc='best')
+        plt.tight_layout()
+        
+        if saveas:
+            plt.savefig(saveas)
+        else:
+            plt.show()
+            
+        
+    def __collect(self, data, labels):
+        tmin = []
+        tmax = []
+        tavg = []
+        
+        for d in data:
+            label = d['what']
+            
+            if label in labels and 'cpu max' in d:
+                tmin.append(d['cpu min'])
+                tmax.append(d['cpu max'])
+                tavg.append(d['cpu avg'])
+        
+        return tmin, tavg, tmax
+    
+    
     def summary_plot(self, fname, **kwargs):
         """
         Create a plot with minimum, maximum and average timings
@@ -15,7 +127,6 @@ class TimePlot:
         ----------
         fname               (str)   timing file
         saveas              (str)   export the summary plot
-        cmap_name='YlGn'    (str)   color scheme
         figsize=(12, 9)             size of the figure
         grid=False          (bool)  show grid
         
@@ -33,16 +144,12 @@ class TimePlot:
         
         # get properties
         saveas = kwargs.get('saveas', None)
-        cmap_name = kwargs.get('cmap', 'YlGn')
         figsize = kwargs.get('figsize', (12, 9))
         grid = kwargs.get('grid', False)
         
         time = timing()
         time.read_ippl_timing(fname)
         data = time.getTiming()
-        
-        cmap = plt.get_cmap(cmap_name)
-        colors = cmap(np.linspace(0, 1, len(data)))
         
         labels = []
         tmin = []
@@ -122,34 +229,7 @@ class TimePlot:
         time.read_ippl_timing(fname)
         data = time.getTiming()
         
-        
-        if not data:
-            raise RuntimeError('No data available.')
-        
-        if prop not in data[1]:
-            raise KeyError('This property is not part of the dictionary.')
-        
-        labels = []
-        times = []
-        
-        if first == None or first > len( data ) - 1:
-            # do all
-            first = len( data ) - 1 # without main timing --> -1
-        elif first < 1:
-            raise RuntimeError("Can't plot the first " + str(first) + " timings.")
-        
-        for d in data:
-            label = d['what']
-            
-            if not 'main' in label:
-                labels.append(label)
-                times.append(d[prop])
-            
-        # 15. Jan. 2017,
-        # http://stackoverflow.com/questions/9543211/sorting-a-list-in-python-using-the-result-from-sorting-another-list
-        times_sorted, labels_sorted = zip(*sorted(zip(times, labels),
-                                                  key=itemgetter(0),
-                                                  reverse=True))
+        times_sorted, labels_sorted = self.__getMostTimeConsuming(first, data, prop)
         
         # 15. Jan. 2017, https://gist.github.com/vals/5257113
         cmap = plt.get_cmap(cmap_name)
@@ -160,7 +240,7 @@ class TimePlot:
 
         # 15. Jan. 2017,
         # http://stackoverflow.com/questions/7082345/how-to-set-the-labels-size-on-a-pie-chart-in-python
-        patches, texts, autotexts = ax.pie(times_sorted[0:first],
+        patches, texts, autotexts = ax.pie(times_sorted,
                                            autopct='%1.1f%%',
                                            pctdistance=0.6,
                                            startangle=90,
@@ -175,7 +255,7 @@ class TimePlot:
         for at in autotexts:
             at.set_fontsize(16)
         
-        ax.legend(patches, labels_sorted[0:first], loc='best', bbox_to_anchor=(1.0, 0.98), borderaxespad=0.1)
+        ax.legend(patches, labels_sorted, loc='best', bbox_to_anchor=(1.0, 0.98), borderaxespad=0.1)
         #plt.tight_layout()
         plt.axis('equal')
         
@@ -183,3 +263,63 @@ class TimePlot:
             plt.savefig(saveas)
         else:
             plt.show()
+            
+    
+    def __getMostTimeConsuming(self, n, data, prop, main=False):
+        """
+        Retturn time and label of the first n most time
+        consuming timings.
+        
+        Parameters
+        ----------
+        n       (int)   number of timings
+        data    ([{}])  timing data of one file
+        prop    (str)   what property should be compared
+                        possible keys:
+                            - cpu max
+                            - cpu avg
+                            - cpu min
+                            - wall max
+                            - wall avg
+                            - wall min
+        main    (bool)  use also main timer
+        
+        Returns
+        -------
+        sorted times and labels
+        """
+        
+        if not data:
+            raise RuntimeError('No data available.')
+        
+        if prop not in data[1]:
+            raise KeyError('This property is not part of the dictionary.')
+        
+        labels = []
+        times = []
+        
+        if n == None or n > len( data ) - 1:
+            # do all
+            n = len( data ) - 1 # without main timing --> -1
+        elif n < 1:
+            raise RuntimeError("Can't plot the first " + str(n) + " timings.")
+        
+        for d in data:
+            label = d['what']
+            
+            # this is really ugly!!!
+            if ('main' in label) and main and (prop in d):
+                labels.append(label)
+                times.append(d[prop])
+            
+            if not 'main' in label:
+                labels.append(label)
+                times.append(d[prop])
+            
+        # 15. Jan. 2017,
+        # http://stackoverflow.com/questions/9543211/sorting-a-list-in-python-using-the-result-from-sorting-another-list
+        times_sorted, labels_sorted = zip(*sorted(zip(times, labels),
+                                                  key=itemgetter(0),
+                                                  reverse=True))
+        
+        return times_sorted[0:n], labels_sorted[0:n]
