9ec23ba8febfcdad4fc20a849b36196d3ae1e337
Matthias Frey <matthias.frey@psi.ch>
2018-03-31 15:54:08 +0200

    add possibility to find bunches

--- opal/analysis/beam.py
+++ opal/analysis/beam.py
@@ -2,6 +2,9 @@
 # Date:     March 2018
 
 from opal.statistics import statistics as stat
+import numpy as np
+import scipy as sc
+from scipy.signal import argrelmin
 
 def halo_continuous_beam(ds, var, **kwargs):
     """
@@ -9,6 +12,15 @@ def halo_continuous_beam(ds, var, **kwargs):
     vertical direction according to
     
     h_x = <x^4> / <x^2>^2 - 2
+    
+    Parameters
+    ----------
+    ds      (DatasetBase)   datasets
+    var     (str)           the variable
+    
+    Optionals
+    ---------
+    see opal.statistics.moment
     
     Reference
     ---------
@@ -30,6 +42,15 @@ def halo_ellipsoidal_beam(ds, var, **kwargs):
     
     h_x = <x^4> / <x^2>^2 - 15 / 17
     
+    Parameters
+    ----------
+    ds      (DatasetBase)   datasets
+    var     (str)           the variable
+    
+    Optionals
+    ---------
+    see opal.statistics.moment
+    
     Reference
     ---------
     T. P. Wangler, Los Alamos National Laboratory, Los Alamos, NM 87545,
@@ -41,3 +62,55 @@ def halo_ellipsoidal_beam(ds, var, **kwargs):
     m2 = stat.moment(ds, var, k=2, **kwargs)
     
     return m4 / m2 ** 2 - 15.0 / 7.0
+
+
+def find_beams(ds, var, **kwargs):
+    """
+    Compute the starting and end points of a beam via
+    kernel density estimation and the calculation of
+    relative minima of the dataset.
+    The purpose of this script is to distinguish bunches
+    of a multi-bunch simulation.
+    
+    Parameters
+    ----------
+    ds      (DatasetBase)   datasets
+    var     (str)           the variable
+    
+    Optionals
+    ---------
+    step    (int)           of dataset
+    npoints (int)           number of points to evaluate
+                            kernel density estimator
+    
+    References (31. March 2018)
+    ---------------------------
+    https://docs.scipy.org/doc/scipy-0.19.0/reference/generated/scipy.stats.gaussian_kde.html
+    https://docs.scipy.org/doc/scipy-0.16.1/reference/generated/scipy.signal.argrelmin.html
+    
+    Returns
+    -------
+    a list of minima locations
+    """
+    
+    step    = kwargs.get('step', 0)
+    npoints = kwargs.get('npoints', 500)
+    
+    data = ds.getData(var, step=step)
+    
+    kde = sc.stats.gaussian_kde(data)
+    
+    xmin = min(data)
+    xmax = max(data)
+    points = np.linspace(xmin, xmax, npoints)
+    
+    pdf = kde.pdf(points)
+    
+    extrema = argrelmin(pdf)
+    
+    bc = [xmin]
+    for idx in extrema[0]:
+        bc.append(points[idx])
+    bc.append(xmax)
+    
+    return bc
