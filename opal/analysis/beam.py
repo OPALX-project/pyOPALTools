53657590ab30c0fd7572013492f82088ac6433bf
Matthias Frey <matthias.frey@psi.ch>
2018-04-01 12:09:58 +0200

    add function to select bunch

--- opal/analysis/beam.py
+++ opal/analysis/beam.py
@@ -95,3 +95,43 @@ def find_beams(ds, var, **kwargs):
     data = ds.getData(var, step=step)
     
     return impl_beam.find_beams(data, **kwargs)
+
+
+def get_beam(ds, var, k, **kwargs):
+    """
+    Obtain the data of a variable of a beam in a
+    multi-bunch simulation.
+    
+    Parameters
+    ----------
+    ds      (DatasetBase)   datasets
+    var     (str)           the variable
+    k       (int)           select k-th bunch
+    
+    Optionals
+    ---------
+    step    (int)           of dataset
+    npoints (int)           number of points to evaluate
+                            kernel density estimator
+    
+    Returns
+    -------
+    the data as an array / list
+    """
+    if not isinstance(ds, DatasetBase):
+        raise TypeError("Dataset '" + ds.filename +
+                        "' not derived from 'DatasetBase'.")
+    
+    if k < 0:
+        raise ValueError("Bunch number has to be 'k >= 0'.")
+    
+    step    = kwargs.get('step', 0)
+    
+    data = ds.getData(var, step=step)
+    
+    minima = impl_beam.find_beams(data, **kwargs)
+    
+    if k > len(minima) - 1:
+        raise ValueError("Bunch number has to be 'k < " + str(len(minima)) + "'.")
+    
+    return data[(data >= minima[k]) & (data < minima[k+1])]
