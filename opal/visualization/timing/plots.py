2f0a9e3371faae3902906fb144d6fca1aa95d2ee
Matthias Frey <matthias.frey@psi.ch>
2018-04-04 09:31:01 +0200

    add speed-up plot

--- opal/visualization/timing/plots.py
+++ opal/visualization/timing/plots.py
@@ -70,6 +70,7 @@ def plot_efficiency(dsets, what, prop, **kwargs):
     yscale  (str)           y-axis scale, 'linear' or 'log'
     grid    (bool)          if true, plot grid
     percent (bool)          efficiency in percentage
+    xlabel  (str)           label for x-axis. Default '#cores'
     
     Returns
     -------
@@ -151,11 +152,138 @@ def plot_efficiency(dsets, what, prop, **kwargs):
     grid   = kwargs.get('grid', False)
     
     plt.plot(cores, efficiency)
-    plt.xlabel('#cores')
+    plt.xlabel(kwargs.get('xlabel', '#cores'))
     plt.ylabel(ylabel)
     plt.xscale(xscale)
     plt.yscale(yscale)
     plt.grid(grid, which='both')
     plt.tight_layout()
     
+    return plt
+
+
+def plot_speedup(dsets, what, prop, **kwargs):
+    """
+    Speedup plot of a timing benchmark study
+    
+    S_p = T_1 / T_p
+    
+    where T_1 is the time for a single core run
+    (or reference run with several cores / nodes)
+    and T_p the time with p cores. S_p then represents the
+    speed-up with p cores / nodes.
+    
+    Parameters
+    ----------
+    dsets   ([DatasetBase]) all timing datasets
+    what    (str)           timing name
+    prop    (str)           property, i.e. 'cpu avg', 'cpu max', 'cpu min',
+                            'wall avg', 'wall max', 'wall min' or
+                            'cpu tot' and 'wall tot' (only for main timing)
+    
+    Optionals
+    ---------
+    xscale      (str)           x-axis scale, 'linear' or 'log'
+    yscale      (str)           y-axis scale, 'linear' or 'log'
+    grid        (bool)          if true, plot grid
+    efficiency  (bool)          add efficiency to plot
+    xlabel      (str)           label for x-axis. Default '#cores'
+    reference   (bool)          add speed-up reference line
+    
+    Returns
+    -------
+    a matplotlib.pyplot handle
+    """
+    if not isinstance(dsets, list):
+        raise TypeError("Input 'dsets' has to be a list for.")
+    
+    if not dsets:
+        raise ValueError("No dataset in 'dsets'.")
+    
+    for ds in dsets:
+        if not isinstance(ds, DatasetBase):
+            raise TypeError("Dataset '" + ds.filename +
+                            "' not derived from 'DatasetBase'.")
+        if not ds.filetype == FileType.TIMING and not ds.filetype == FileType.OUTPUT:
+            raise TypeError("Dataset '" + ds.filename +
+                            "' is not a timing dataset.")
+    
+    cores = []
+
+    # find timing dictionary of corresponding property 'prop'
+    # 'idx' will be set accordingly
+    idx = 0
+    match = False
+    available = []
+    for timing in dsets[0].getData(''):
+        if timing['what'] == what:
+            match = True
+        else:
+            available.append( timing['what'] )
+            idx += 1
+    
+    if not match:
+        raise ValueError("No timing called '" + what + "'. Possible entries:"
+                         + str(available))
+    
+    # clear again
+    available = []
+    
+    time = []
+    
+    for ds in dsets:
+        data = ds.getData('')
+        
+        #access main timing
+        cores.append( int(data[0]['cores']) )
+        
+        time.append( data[idx][prop] )
+    
+    # sort
+    cores, time = zip(*sorted(zip(cores, time)))
+    
+    
+    # obtain speed-up
+    speedup = []
+    for t in time:
+        speedup.append( time[0] / t )
+    
+    xscale = kwargs.get('xscale', 'linear')
+    yscale = kwargs.get('yscale', 'linear')
+    grid   = kwargs.get('grid', False)
+    
+    plt.plot(cores, speedup)
+    plt.xlabel(kwargs.get('xlabel', '#cores'))
+    plt.ylabel('speed-up')
+    plt.xscale(xscale)
+    plt.yscale(yscale)
+    plt.grid(grid, which='both')
+    plt.tight_layout()
+    
+    if kwargs.get('reference', False):
+        ref = []
+        for c in cores:
+            ref.append( c / cores[0] )
+        plt.plot(cores, ref, 'k--', label='reference')
+        plt.legend()
+    
+    
+    if kwargs.get('efficiency', False):
+        # obtain core increase
+        incr = []
+        for c in cores:
+            incr.append( c / cores[0] )   
+        
+        # obtain efficiency
+        efficiency = []
+        
+        ax2 = plt.twinx()
+        ax2.set_ylabel('efficiency', color='r')
+        ax2.set_yscale(yscale)
+        
+        for i, s in enumerate(speedup):
+            efficiency.append( s / incr[i] )
+        
+        ax2.plot(cores, efficiency, 'r--')
+        
     return plt
