b7f0129ae8243c3d9f17e597242564bd3e18b8fb
nneveu <nneveu@hawk.iit.edu>
2018-04-09 14:07:07 -0500

    removing padded zeros from bounded db

--- db/mldb.py
+++ db/mldb.py
@@ -76,44 +76,67 @@ def buildBounded(pickle, baseFN):
     totalgen  = dbr.getNumberOfSamples()  
     bounded   = []
     unbounded = [] 
+    xvec  = np.zeros((1, n))
+    bxvec = np.zeros((1, n))
+    objsNames  = dbr.getYNames()
+    yvec  = np.zeros((1, len(objsNames)))
+    byvec = np.zeros((1, len(objsNames)))
+
     #Loop through each generation
     for gen in range(0, totalgen):
-        nsims = dbr.getSampleSize(i=gen)
-                #Save extra info            
+        nsims  = dbr.getSampleSize(i=gen)
+        gxvec  = np.zeros((1, n))
+        gyvec  = np.zeros((1, len(objsNames)))
+        #Save extra info            
         if (gen == 0):
-            objsNames  = dbr.getYNames()
-            bounded.append({'sampleSize':totalgen,
+                        bounded.append({'sampleSize':totalgen,
                             'dvarNames' :keys,
                             'objNames'  :objsNames,
                             'bounds'    :ulb})
 
-        xvec  = np.zeros((nsims, n))
-        bxvec = np.zeros((nsims, n))
-        yvec  = np.zeros((nsims, len(objsNames)))
-        byvec = np.zeros((nsims, len(objsNames)))
-        #Loop through each simulation in gen 
+                #Loop through each simulation in gen 
         for x in range(0,nsims):
-            xvals  = dbr.getDVarVec(gen,x) 
+            xvals  = dbr.getDVarVec(gen,x)
             ovals  = dbr.getObjVec(gen,x)
-            #Check if xvals <= lb
             testlb = np.less_equal(xvals, lb)
-            #Check if xvlas >= ub
             testub = np.greater_equal(xvals, ub)
-
+            #Check if xvals <= lb or xvlas >= ub
             if (any(testlb) == True) or (any(testub) == True):  
                 badpts  = badpts +1
-                bxvec[x,:] = xvals
-                byvec[x,:] = ovals
+                xvals = xvals.reshape((1,8))
+                ovals = ovals.reshape((1,4))
+                bxvec = np.append(bxvec, xvals, axis=0)
+                byvec = np.append(byvec, ovals, axis=0)
+            #Check if xvlas within all bounds
             elif (all(testlb) == False) and (all(testub) == False):
                 goodpts = goodpts +1
-                xvec[x,:] = xvals
-                yvec[x,:] = ovals
+                xvals = xvals.reshape((1,8))
+                ovals = ovals.reshape((1,4))
+                xvec = np.append(xvec, xvals, axis=0)
+                yvec = np.append(yvec, ovals, axis=0)
+                gxvec = np.append(gxvec, xvals, axis=0)
+                gyvec = np.append(gyvec, ovals, axis=0)
+
             else:
                 print('Mistake, xvals not in boundaries expected.')
+                print('Don\'t trust the database.')
+        
+        gxvec = gxvec[1:,:]
+        gyvec = gyvec[1:,:]
+        #Saving good pts per generation
+        bounded.append({'dvarValues':gxvec,'objValues' :gyvec})
 
-        bounded.append({'dvarValues':xvec,'objValues' :yvec})
-        unbounded.append({'dvarValues':bxvec, 'objValues' :byvec})
- 
+    #Getting rid of place holders
+    xvec = xvec[1:,:]
+    yvec = yvec[1:,:]
+    bxvec = bxvec[1:,:]
+    byvec = byvec[1:,:]
+    #Saving all data in one entry
+    bounded.append({'allDvarValues':xvec, 'allObjValues':yvec})
+    #Saving all bad points, looses generation info
+    unbounded.append({'dvarValues':bxvec, 'objValues' :byvec})
+    print(np.shape(xvec), np.shape(bxvec), np.shape(gxvec), gxvec) 
+
     print('# bad pts:', badpts, '# good pts:', goodpts)
     #badbounds = checkBounds(unbounded, keys)
 
