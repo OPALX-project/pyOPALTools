8cf1a7cbdbd1ae3b05471c28648377da713909b4
Matthias Frey <matthias.frey@psi.ch>
2018-05-03 13:16:34 +0200

    update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,3 +3,5 @@ from utilities.H5Error import *
 from utilities.H5Parser import *
 from utilities.LatticeParser import *
 from utilities.TrackOrbitParser import *
+from utilities.HistogramParser import *
+from utilities.PeakParser import *
