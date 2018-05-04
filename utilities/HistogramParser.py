c1f2fa8e673a3f6ec44581507fa7e87a32364fe6
Matthias Frey <matthias.frey@psi.ch>
2018-05-04 16:34:37 +0200

    parse histogram measurement file

--- utilities/HistogramParser.py
+++ utilities/HistogramParser.py
@@ -7,7 +7,17 @@ import numpy as np
 class HistogramParser:
     """
     Parses *.hist files of OPAL that are written for probe
-    elements.
+    elements and *.hist files of measurements.
+    
+    The first line in both files is a comment line starting with
+    '#'. The measurement header line should be
+    
+    # Histogram bin counts (radius ['unit'], intensity ['unit'])
+    
+    where a simulation header is
+    
+    # Histogram bin counts (min, max, nbins, binsize) \
+        'min' 'unit' 'max' 'unit' '#bins' 'binsize 'unit'
     
     Members
     -------
@@ -33,9 +43,16 @@ class HistogramParser:
     
     def parse(self, filename):
         
-        pattern = r'# Histogram bin counts \((\w+), (\w+), (\w+), (\w+)\) (.*) (\w+) (.*) (\w+) (\d+) (.*) (\w+)'
+        simulation_pattern = r'# Histogram bin counts \((\w+), (\w+), (\w+), (\w+)\) (.*) (\w+) (.*) (\w+) (\d+) (.*) (\w+)'
+        measurement_pattern = r'# Histogram bin counts \((\w+) \[(\w+)\], (\w+) \[(\w+)\]\)'
         
         match = False
+        
+        # measurement file
+        radius  = None
+        runit   = None
+        current = None
+        cunit   = None
         
         with open(filename) as f:
             for line in f:
@@ -43,7 +60,7 @@ class HistogramParser:
                     # 24. March
                     # https://stackoverflow.com/questions/2077897/substitute-multiple-whitespace-with-single-whitespace-in-python
                     line = ' '.join(line.split())
-                    obj = re.match(pattern, line)
+                    obj = re.match(simulation_pattern, line)
                     
                     if obj:
                         j = 5
@@ -63,18 +80,44 @@ class HistogramParser:
                                 j += 1
                         
                         match = True
+                    
+                    obj = re.match(measurement_pattern, line)
+                    if obj:
+                        radius  = obj.group(1)
+                        runit   = obj.group(2)
+                        current = obj.group(3)
+                        cunit   = obj.group(4)
+                        
+                        match = True
+                        
                     else:
                         break
         
         if not match:
             raise RuntimeError('Not proper file format.')
         
-        self._info['bincount'].append( np.genfromtxt(filename,
-                                                     comments='#',
-                                                     delimiter = ' ',
-                                                     dtype=np.float64
-                                                     )
-        )
+        
+        if not radius == None:
+            # measurement file
+            r, c = np.loadtxt(filename, skiprows=1, unpack=True)
+            self._info['nbins'] = [len(r), '']
+            self._info['min'] = [min(r), runit]
+            self._info['max'] = [max(r), runit]
+            
+            self._info['binsize'] = [
+                (self._info['max'][0] - self._info['min'][0]) / self._info['nbins'][0],
+                runit
+            ]
+            
+            self._info['bincount'].append( c )
+        
+        else:
+            self._info['bincount'].append( np.genfromtxt(filename,
+                                                         comments='#',
+                                                         delimiter = ' ',
+                                                         dtype=np.float64
+                                                        )
+            )
         
         self._info['bincount'].append('')
     
