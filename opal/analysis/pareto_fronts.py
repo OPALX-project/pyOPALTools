11329bb37bdb14641326686d76465823e2a81c51
ext-neveu_n <nneveu@hawk.iit.edu>
2018-05-23 15:23:36 -0500

    adding function to read mldb databases

--- opal/analysis/pareto_fronts.py
+++ opal/analysis/pareto_fronts.py
@@ -1,21 +1,100 @@
 # Author:   Nicole Neveu 
 # Date:     May 2018
 
-from opal.datasets.filetype import FileType
-from opal.statistics import statistics as stat
-from opal.datasets.DatasetBase import DatasetBase
-from opal.analysis import impl_beam
+
 import numpy as np
-
-import json
-import pylab as pl
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from opal.datasets.filetype import FileType
+from db import mldb
 
-from collections import OrderedDict
-from optPilot.Annotate import AnnoteFinder
-import pyOPALTools.optPilot.OptPilotJsonReader as jsonreader
+def pareto(x, y, dvars=0):
+    """
+    Find Pareto points for 2 objectives, given
+    all data recorded by optimization run. 
+    These points are calculated independent
+    of generation. i.e. best points from all 
+    generations are found and saved.
+
+    Parameters 
+    ----------
+    x   (numpy array)   1D array of first objective values
+    y   (numpy array)   1D array of second objective values
+    
+    Optionals
+    ---------
+    dvars   (numpy array)   ND array of design variables 
+
+    Returns
+    -------
+    pfdict (dictionary) Dictionary that holds pareto front
+                        values and corresponding design values
+    """
+    #Check data is correct length
+    lx = len(x)
+    ly = len(y)
+    ld = len(dvars[:,0])
+    if lx==ly==ld:
+        pass
+    else: 
+        print('Input data sizes do not match\n')
+        print('Please check input arrays')
+    
+    #Making holders for my pareto fronts            
+    pareto_y = []
+    pareto_x = []
+    pdvar    = []
+    w  = np.arange(0,1.001, 0.001)
+    sx = scaleData(x)
+    sy = scaleData(y)
+    
+    #Finding best point with respect to all weights (w)
+    for i in range(0, len(w)):
+        fobj     = sy * w[i] + sx *(1-w[i])
+        wmins    = np.where(fobj==min(fobj))[0][0]
+        pareto_y = np.append(pareto_y, y[wmins])
+        pareto_x = np.append(pareto_x, x[wmins])
+
+    pareto_pts = delete_repeats(pareto_x, pareto_y)
+    ind        = np.array(pareto_pts.index.tolist())
+
+    #Check dvars is correct length
+    if dvars!=0:
+        pdvar      = dvars[ind, :]
+
+    return(pareto_pts.ix[:,0], pareto_pts.ix[:,1], pdvar) #pareto_x, pareto_y, pdvar)
+
+
+def get_all_data_db(dbpath):
+    """
+    Get all objectives and design variables
+    from every generation in an optimzation
+    database. Databases are made using OPAL 
+    output from json files or stat files. 
+    Functions to make databases can be found
+    in mldb.py. 
+    
+    Parameters 
+    ----------
+    db  (str)   path to pickle file containing 
+                database made with mldb.py
+
+    Returns
+    -------
+    data    (dict)  Dictonary containing all 
+                    objectives and design values
+                    in optimization database.
+    """
+    data = {}
+    dbr = mldb.mldb()
+    dbr.load(dbpath)
+    #dvars  = dbr.getXNames()
+    #obj    = dbr.getYNames()
+    gens   = dbr.getNumberOfSamples()
+
+    
+    return(data)
+
+
 
 
 def scaleData(vals):
@@ -36,44 +115,25 @@ def scaleData(vals):
     scaled_vals = (vals - smin)/smax
     return (scaled_vals)
 
-def pareto(x, y, dvars=0):
-    """
-    Find Pareto points for 2 objectives given
-    all data recorded by optimization run. 
-    These points are calculated independent
-    of generation. i.e. best points from all 
-    generations are found and saved.
 
-    Parameters 
-    ----------
-    x   (numpy array)   array of first objective values
-    y   (numpy array)   array of second objective values
+def delete_repeats(x, y, z=0):
+    """
+    Delete repeated pareto front values, if any.
     
+    Parameters
+    ----------
+    x   (numpy array)   1D array of first objective values
+    y   (numpy array)   1D array of second objective values
+   
     Optionals
     ---------
-    dvars
+    z   (numpy array)   ND array of second design variables
+
     """
-    #Making holders for my pareto fronts            
-    pareto_y = []
-    pareto_x = []
-    pdvar    = []
-    w  = np.arange(0,1.001, 0.001)
-    sx = scaleData(x)
-    sy = scaleData(y)
-    #Finding best point with respect to all weights (w)
-    for i in range(0, len(w)):
-        fobj     = sy * w[i] + sx *(1-w[i])
-        wmins    = np.where(fobj==min(fobj))[0][0]
-        pareto_y = np.append(pareto_y, y[wmins])
-        pareto_x = np.append(pareto_x, x[wmins])
-
-    pareto_pts = delete_repeats(pareto_x, pareto_y)
-    ind        = np.array(pareto_pts.index.tolist())
-    pdvar      = dvars[ind, :]
-
-    return(pareto_pts.ix[:,0], pareto_pts.ix[:,1], pdvar) #pareto_x, pareto_y, pdvar)
-
-def delete_repeats(x, y): #, z):
-    df = pd.DataFrame({'x':x, 'y':y}) #, 'z':z})
+    if z==0:
+        df = pd.DataFrame({'x':x, 'y':y}) #, 'z':z})
+    else:
+        df = pd.DataFrame({'x':x, 'y':y, 'z':z})
+    
     return df.drop_duplicates(subset=['x', 'y'], keep='first')
 
