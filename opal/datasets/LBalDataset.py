bb468966d0ebcb316b47d9261ac7ce27eb17247e
Matthias Frey <matthias.frey@psi.ch>
2019-03-25 14:57:00 +0100

    Issue #19: Load balancing dataset

--- opal/datasets/LBalDataset.py
+++ opal/datasets/LBalDataset.py
@@ -127,3 +127,28 @@ class LBalDataset(DatasetBase):
         list of strings
         """
         return self.__parser.getVariables()
+
+
+    @property
+    def size(self):
+        return self.__parser.size
+
+    def __str__(self):
+        variables = self.__parser.getVariables()
+        n = len(variables)
+        s  = '\n\tLoad balancing dataset.\n\n'
+        s += '\tSize: ' + str(n) + ' x ' + str(self.size) + '\n\n'
+        s += '\tAvailable variables (' + str(n) + ') :\n\n'
+        vv = sorted(variables)
+        if n < 11:
+            for v in vv:
+                s += '\t' + '%-20s' % (v) + '\t' + self.__parser.getDescriptionOfVariable(v) + '\n'
+        else:
+            s += '\t' + '%-20s' % (vv[0]) + '\t' + self.__parser.getDescriptionOfVariable(vv[0]) + '\n'
+            s += '\t' + '%-20s' % (vv[1]) + '\t' + self.__parser.getDescriptionOfVariable(vv[1]) + '\n'
+            s += '\t' + '%-20s' % (vv[2]) + '\t' + self.__parser.getDescriptionOfVariable(vv[2]) + '\n'
+            s += '\t...\n'
+            s += '\t' + '%-20s' % (vv[-2]) + '\t' + self.__parser.getDescriptionOfVariable(vv[-2]) + '\n'
+            s += '\t' + '%-20s' % (vv[-1]) + '\t' + self.__parser.getDescriptionOfVariable(vv[-1]) + '\n'
+            
+        return s
