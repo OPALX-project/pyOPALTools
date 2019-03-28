364ebf29a81458a4c9bc9442458819ed2cae23d7
Matthias Frey <matthias.frey@psi.ch>
2019-03-28 16:29:59 +0100

    function not needed

--- opal/visualization/BasePlotter.py
+++ opal/visualization/BasePlotter.py
@@ -4,11 +4,3 @@ class BasePlotter:
     
     def __init__(self):
         pass
-    
-    @property
-    def ds(self):
-        """
-        Implemented in each dataset. Each dataset
-        has to return self.
-        """
-        pass
