d774d207905dd8a1741d7c5bf830fb23941a531f
Matthias Frey <matthias.frey@psi.ch>
2020-04-08 17:46:00 +0200

    add file header to analysis classes

--- opal/analysis/pareto_fronts.py
+++ opal/analysis/pareto_fronts.py
@@ -1,5 +1,15 @@
-# Author:   Nicole Neveu
-# Date:     May 2018
+# Copyright (c) 2018, Nicole Neveu, SLAC National Accelerator Laboratory
+# All rights reserved
+#
+# This file is part of pyOPALTools.
+#
+# pyOPALTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# You should have received a copy of the GNU General Public License
+# along with pyOPALTools. If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
 import pandas as pd
